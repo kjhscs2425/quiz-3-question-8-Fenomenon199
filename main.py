a7f6f143db301757ebacd2ede3157695d71ed272
Fenomenon199 <afeldman2025@kehillahstudent.org>
2025-04-30 12:28:38 -0700

    quiz 3

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import json
-
 # read `expenses.json`
+def_average: ("prices")
+print: def_average 
+total: 115 
+    # return the average of a list of numbers
+
 
 # get and print total "price" for all expenses at the "pet store"
